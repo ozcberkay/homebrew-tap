cc22232df1c739299e20836482c44405e9c710fb
Berkay Özcan <ozc.berkay@gmail.com>
2026-01-10 01:43:19 +0300

    Update auralyrics.rb

--- Casks/auralyrics.rb
+++ Casks/auralyrics.rb
@@ -1,6 +1,6 @@
 cask "auralyrics" do
   version "1.0.0"
-  sha256 "6fe844b0daf9a29a9a7bb21b6e885bfef18910e5197b56f1b0eb998bc69674fd"
+  sha256 "03f49136ff2dcceba442328c3bb479201b76861246413f82850ec5c43e117ea3"
 
   url "https://github.com/ozcberkay/AuraLyrics/releases/download/v#{version}/AuraLyrics.tar.gz"
   name "AuraLyrics"
