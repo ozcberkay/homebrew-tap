4aa5f3d4e7d8d3875cf7f566671e88aefba44fcb
Berkay Özcan <ozc.berkay@gmail.com>
2026-01-10 02:22:34 +0300

    Update auralyrics.rb

--- Casks/auralyrics.rb
+++ Casks/auralyrics.rb
@@ -1,20 +1,22 @@
 cask "auralyrics" do
   version "1.0.1"
-  sha256 "03f49136ff2dcceba442328c3bb479201b76861246413f82850ec5c43e117ea3"
+  sha256 "9567a940a8981cc821ebb5f4a66f50d884805c85451c010f98f9d0ae4f954797"
 
   url "https://github.com/ozcberkay/AuraLyrics/releases/download/v#{version}/AuraLyrics.tar.gz"
   name "AuraLyrics"
   desc "Minimalist, Always-on-Top Spotify Lyrics for macOS"
   homepage "https://github.com/ozcberkay/AuraLyrics"
+
   app "AuraLyrics.app"
+
   postflight do
     system_command "xattr",
                    args: ["-rd", "com.apple.quarantine", "#{appdir}/AuraLyrics.app"],
                    sudo: false
   end
+
   zap trash: [
     "~/Library/Application Support/com.berkayozcan.AuraLyrics",
     "~/Library/Preferences/com.berkayozcan.AuraLyrics.plist",
   ]
 end
-
